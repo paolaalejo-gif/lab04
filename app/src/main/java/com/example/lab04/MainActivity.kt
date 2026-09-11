0c82831abeb778f9046eb47df3fd68424e08bd8a
Paola Alejo <paola.alejo@tecsup.edu.pe>
2026-09-11 12:55:14 -0500

    Añadido componente OutlinedTextField

--- app/src/main/java/com/example/lab04/MainActivity.kt
+++ app/src/main/java/com/example/lab04/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
@@ -40,5 +41,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             Text("Mi Botón")
         }
         Switch(checked = true, onCheckedChange = {})
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            label = { Text("Escribe algo") }
+        )
     }
 }
